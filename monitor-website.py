2e38d760a0e8cfff1f90c52f26544acecee9cc9d
Ibrahim.Osama <iosama.amin@gmail.com>
2024-08-03 21:13:34 +0300

    added monitor-website.py

---
+++ monitor-website.py
@@ -0,0 +1,19 @@
+import requests
+import schedule
+
+def monitor_application():
+    try:
+        response = requests.get('http://192.168.111.128:8080/', timeout=5)
+        print(response.status_code)
+        if response.status_code == 200:
+            print("Application is running well.....")
+
+        else:
+            print(f"APPLICATION DOWN. FIX IT with error code {response.status_code}")
+    except Exception as ex:
+        print("The application is unreachable. It is a network issue")
+
+
+schedule.every(5).seconds.do(monitor_application)
+while True:
+    schedule.run_pending()
